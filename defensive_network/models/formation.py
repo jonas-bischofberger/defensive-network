9af3e9cd6b6b52721c651821e34c6252e97ce435
jonas-bischofberger <49710218+jonas-bischofberger@users.noreply.github.com>
2025-05-29 11:39:09 +0200

    Update

--- defensive_network/models/formation.py
+++ defensive_network/models/formation.py
@@ -393,3 +393,4 @@ def main1():
         st.write(_plot_roles(df_tracking_formation, role_name_col="role_category"))
         st.write(_plot_roles(df_tracking_formation, role_name_col="role"))
         st.write("-------------")
+
